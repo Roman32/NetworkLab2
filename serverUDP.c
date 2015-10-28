b28ed5a27d00c6c83e380d03ffaa093c51f84bb2
Roman <claprood.8@osu.edu>
2015-10-28 09:36:03 -0400

    start of server

--- serverUDP.c
+++ serverUDP.c
@@ -2,6 +2,10 @@
 #include <netinet/in.h>
 #include <stdio.h>
 
+/*Authors: Eric Olson and Roman Claprood
+Project: Lab2 Client/Server UDP with RDT 3.0
+Due Date: November 4, 2015
+*/
 
 int int main(int argc, char *argv[])
 {
@@ -10,12 +14,30 @@ int int main(int argc, char *argv[])
 	socklen_t length;
 	char response[1000];
 
-	sock = socket(PF_INET,SOCK_DGRAM,0);
-	
-	while(1){
-		length = sizeof(client);
-		
+	if (argc < 2)
+	{
+		printf("Error, please give a port!\n");
+		exit(1);
 	}
+
+	sock = socket(PF_INET,SOCK_DGRAM,0);
+	if(sock < 0){
+		printf("Error opening socket!");
+		exit(0);
+	}
+
+	server.sin_family = PF_INET;
+	server.sin_addr.s_addr = htonl(INADDR_ANY);
+	server.sin_port = htons(atoi(argv[1]));
+	if(bind(sock,(struct sockaddr*)&server,sizeof(server)) < 0){
+		printf("Error binding socket!\n");
+		exit(0);
+	}
+
+
+	
 
 	return 0;
 }
+
+
