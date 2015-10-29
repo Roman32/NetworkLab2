a9439864059fe364f88636ec9c61d8e4e71f249a
Roman <claprood.8@osu.edu>
2015-10-29 11:39:43 -0400

    add

--- serverUDP.c
+++ serverUDP.c
@@ -1,21 +1,27 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*Authors: Eric Olson and Roman Claprood
 Project: Lab2 Client/Server UDP with RDT 3.0
 Due Date: November 4, 2015
 */
 
-int int main(int argc, char *argv[])
+void getFileName();
+
+int main(int argc, char *argv[])
 {
 	int sock,check;
 	struct sockaddr_in server,client;
 	socklen_t length;
-	char response[1000];
+	char request[256];
+	char header[256];
+	char filename[50];
 
-	if (argc < 2)
-	{
+
+	if (argc < 2){
 		printf("Error, please give a port!\n");
 		exit(1);
 	}
@@ -29,15 +35,35 @@ int int main(int argc, char *argv[])
 	server.sin_family = PF_INET;
 	server.sin_addr.s_addr = htonl(INADDR_ANY);
 	server.sin_port = htons(atoi(argv[1]));
+
 	if(bind(sock,(struct sockaddr*)&server,sizeof(server)) < 0){
 		printf("Error binding socket!\n");
 		exit(0);
 	}
 
+	//Gets
+	while(1){
+		length = sizeof(client);
+		check = recvfrom(sock,request,256,0,(struct sockaddr*)&client,&length);
 
-	
+	}
 
 	return 0;
+}
+
+//Used to parse response to get file name
+void getFileName(char request[],char filename[]){
+	int i = 0;
+	char *strArray[50];
+	char *token = strtok(request, " \n\r");
+	//pares the message
+	while(token != NULL){
+		strArray[i] = malloc(strlen(token)+1);
+		strcpy(strArray[i],token);
+		i++;
+		token = strtok(NULL, " \n\r");
+	}
+	strcpy(filename,strArray[i]);
 }
 
 
